b42d9c52cdeeaeb8c111553f79ff0736c3add8db
Prince Kumar Dhankhar <71747203+pdhankhar61@users.noreply.github.com>
2021-08-25 03:52:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('push', function (event) {
 	if (event.data) {
 		self.registration.showNotification(event.data.text(), {
 			body: "Do you know him",
-			icon: "https://media-exp1.licdn.com/dms/image/C5103AQHpZcfxAYeeTg/profile-displayphoto-shrink_100_100/0/1578474575067?e=1635379200&v=beta&t=wwQBkafk62bupGx0lneWA5xhgIAYtGmiWho7plsI3LM",
+			icon: "add your own link",
 			vibrate: [200, 100, 200, 100, 200, 100, 400],
 			tag: "request",
 			actions: [
